05717fa21d690aa2b091013c894c522aff248402
johan <johannguillot@hotmail.com>
2024-03-07 11:21:52 +0100

    installing firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBd0ZDHS0ILXPc_56jARtTjL8FwLJC6tQc",
+  authDomain: "madejoh-first-project.firebaseapp.com",
+  projectId: "madejoh-first-project",
+  storageBucket: "madejoh-first-project.appspot.com",
+  messagingSenderId: "1018104081453",
+  appId: "1:1018104081453:web:64d4a23fb34cc9cea51ca4",
+};
+
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Seo title="MaDeJoH" description="Description" />
+      <h1>madejoh web app</h1>
     </>
   );
 }
